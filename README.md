9d6304cab750e0a8d89c92f1a9384d6ad99e8ab3
suyang.li <suyang.li@percent.cn>
2022-05-16 11:08:19 +0800

    react初始化配置

---
+++ README.md
@@ -0,0 +1,3 @@
+# react 模板
+
+webpack 自定义配置 react 18
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from './pages/Home.jsx'
+const App = () => {
+  return (
+    <div>
+      hello world
+      <Home />
+    </div>
+  )
+}
+export default App
