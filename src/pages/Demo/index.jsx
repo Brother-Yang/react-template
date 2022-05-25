0be5b6f63fd8d86315bcc88eb4dc62286f9ee5ce
suyang.li <suyang.li@percent.cn>
2022-05-25 18:01:49 +0800

    简单配置eslint

--- src/pages/Demo/index.jsx
+++ src/pages/Demo/index.jsx
@@ -8,21 +8,19 @@ const Demo = () => {
   const dispatch = useDispatch()
   return (
     <div>
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <span className={styles.textNum}>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-      </div>
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span className={styles.textNum}>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
     </div>
   )
 }
