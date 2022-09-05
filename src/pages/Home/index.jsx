a0ed6fa393e8a8b615a2fe4ef8f08a2233c87767
suyang.li <suyang.li@percent.cn>
2022-09-05 11:05:53 +0800

    feat: hello world

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,9 @@
 import React from 'react'
-import { useNavigate, Outlet } from 'react-router-dom'
-import { Button } from 'antd'
 
 import styles from './index.less'
 
 const Home = () => {
-  const navigate = useNavigate()
-  const go = () => {
-    navigate('/demo')
-  }
-  return (
-    <div className={styles.box}>
-      我是home页
-      <Button type="primary">点击</Button>
-      <div onClick={go} className={styles.text}>
-        点击
-      </div>
-      <Outlet />
-    </div>
-  )
+  return <div className={styles.box}>hello world</div>
 }
 
 export default Home
