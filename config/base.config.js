597daf55a49c026aa9584239a739641d3354c3dd
Brother-Yang <450028105@qq.com>
2023-05-11 20:29:02 +0800

    perf(代码): 优化压缩

--- config/base.config.js
+++ config/base.config.js
@@ -2,6 +2,9 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   entry: path.join(__dirname, './../src/index.tsx'),
@@ -15,6 +18,7 @@ module.exports = {
     alias: {
       '@': path.join(__dirname, './../src'),
     },
+    mainFields: ['jsnext:main', 'browser', 'main'],
   },
   module: {
     rules: [
@@ -22,7 +26,7 @@ module.exports = {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader?cacheDirectory',
         },
       },
       // {
@@ -98,4 +102,52 @@ module.exports = {
     new MiniCssExtractPlugin({ filename: '[name].[contenthash:8].css' }),
     new CleanWebpackPlugin(),
   ],
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          // 构建时去除注释
+          format: {
+            comments: false,
+          },
+        },
+        // 注释剥离到单独文件
+        extractComments: false,
+      }),
+
+      new CssMinimizerPlugin(),
+
+      // Scope Hoisting
+      new webpack.optimize.ModuleConcatenationPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      /**
+       * initial 入口chunk，对于异步导入的文件不处理
+       * async 异步chunk，只对异步导入的文件处理
+       * all 全部chunk
+       */
+      // 缓存分组
+      cacheGroups: {
+        // 第三方模块
+        vendor: {
+          name: 'vendor', // chunk 名称
+          priority: 1, // 权限更高，优先抽离，重要！！！
+          test: /node_modules/,
+          minSize: 0, // 大小限制
+          minChunks: 1, // 最少复用过几次
+        },
+
+        // 公共的模块
+        common: {
+          name: 'common', // chunk 名称
+          priority: 0, // 优先级
+          minSize: 0, // 公共模块的大小限制
+          minChunks: 2, // 公共模块最少复用过几次
+        },
+      },
+    },
+  },
 }
