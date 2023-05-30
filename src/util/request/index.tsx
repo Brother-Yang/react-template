f70bb23aed66c5ebf3068123032d3e1924511fec
Brother-Yang <450028105@qq.com>
2023-05-30 16:41:28 +0800

    feat(axios): 携带token

--- src/util/request/index.tsx
+++ src/util/request/index.tsx
@@ -18,8 +18,10 @@ function errorStatus() {
 // 添加请求拦截器
 baseRequest.interceptors.request.use(
   function (config) {
-    // 在发送请求之前做些什么
-    // console.log(config, 'config')
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
     return config
   },
   function (error) {
