a729a3e6075ab02d72410bdaf48a7673a37c95f9
Brother-Yang <suyang.li@percent.cn>
2023-06-15 09:51:59 +0800

    feat: css配置，目前热更新好使

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+import './index.css';
+
+import styles from './index.less';
 
 const Home = () => {
-  return <div>我是home页</div>
-}
+  return (
+    <div>
+      我是home页
+      <input type="text" />
+      <span>hello world</span>
+      <h1 className={styles.test}>hello world</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
@@ -22,6 +23,37 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              // 开启css模块化
+              modules: {
+                namedExport: false,
+                localIdentName: '[path][name]-[local]--[contenthash:base64:5]',
+              },
+            },
+          },
+          // 'postcss-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
@@ -29,5 +61,6 @@ module.exports = {
       title: 'react-template',
       template: './public/index.html',
     }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash:8].css' }),
   ],
 };
