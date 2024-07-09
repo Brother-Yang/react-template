9a3bec35a356f1482ecbbc716b1b033f619a0fb2
suyang.li <450028105@qq.com>
2024-07-09 10:27:24 +0800

    feat: corejs

--- src/config/dict/Dict/dict.system.config.ts
+++ src/config/dict/Dict/dict.system.config.ts
@@ -3,6 +3,7 @@ import Dict from '../index';
 export default {
   initStatic() {
     Dict.handlers.SystemHelloReact = () => {
+      console.log([1, 2, 3].at(2), [2].includes(2), Object.keys({ a: 1 }), '嘿嘿嘿');
       console.log('SystemHelloReact', '我晕行了我晕行了');
       return 'hello react';
     };
